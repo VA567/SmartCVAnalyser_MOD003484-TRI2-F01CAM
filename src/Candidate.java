fe93339d5b6ccc6e4ad55843b6fea240de72d641
vican <Vicand0502@gmail.com>
2025-04-25 21:24:57 +0100

    Candidate.java can store the information about the Candidate
    SmartCVAnalyser.java has basic testing functionality to test the Candidate.java file

--- src/Candidate.java
+++ src/Candidate.java
@@ -1,2 +1,28 @@
+//Importing packages
+import java.util.ArrayList; //This package is needed to create and use ArrayLists
+
 public class Candidate {
+    //Variables
+    public String name; //Stores the candidate's full name as a string
+    public String email; //Stores the candidate's email as a string
+    public String education; //Stores the candidate's education as a string
+    public int yearsOfExperience; //Stores the number of years of experience as an integer
+    public ArrayList<String> skills; //Stores all the candidate's skills as a list
+
+    public Candidate(String name, String email, String education, int yearsOfExperience, ArrayList<String> skills) {
+        this.name = name;
+        this.email = email;
+        this.education = education;
+        this.yearsOfExperience = yearsOfExperience;
+        this.skills = skills;
+    }
+
+    public void displayInfo(){
+        System.out.println("Candidate Name: " + name);
+        System.out.println("Email Address: " + email);
+        System.out.println("Education: " + education);
+        System.out.println("Years of Experience: " + yearsOfExperience);
+        System.out.println("Skills: " + skills);
+    }
+
 }
--- src/SmartCVAnalyser.java
+++ src/SmartCVAnalyser.java
@@ -1,2 +1,12 @@
+import java.util.ArrayList;
+
 public class SmartCVAnalyser {
+    public static void main(String[] args) {
+        ArrayList<String> skills = new ArrayList<String>();
+        skills.add("Java");
+        skills.add("Python");
+        skills.add("Excel");
+        Candidate c1Test = new Candidate("Victor A", "vicand0502@test.com", "BSc Cyber Sec", 1, skills);
+        System.out.println(c1Test.name);
+    }
 }
