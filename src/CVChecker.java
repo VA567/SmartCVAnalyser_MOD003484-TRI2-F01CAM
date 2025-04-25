e6737b9ed5a52f989eb68288f1e40aaeece56682
vican <Vicand0502@gmail.com>
2025-04-26 00:46:56 +0100

    CVChecker.java - Add CVChecker class - Add scoring method - Add MaxScore Variable that stores and calculates the maximum possible score

--- src/CVChecker.java
+++ src/CVChecker.java
@@ -1,2 +1,22 @@
 public class CVChecker {
+    //Calculates a score based on how well the candidate matches the job description
+    public static int calculateMatchScore(Candidate candidate, JobDescription job){
+        //Variables
+        int score = 0; //Stores the raw score as an integer
+        int maxScore = (job.requiredSkills.size() * 10) + 10; //Calculates and stores the maximum score as an integer
+
+        //Calculates the score for the skills
+        for (String requiredSkill : job.requiredSkills) {
+            if (candidate.skills.contains(requiredSkill)) {
+                score += 10;
+            }
+        }
+
+        //Calculates score for the experience
+        if (candidate.yearsOfExperience >= job.requiredExperienceYears) {
+            score += 10;
+        }
+
+        return score;
+    }
 }
